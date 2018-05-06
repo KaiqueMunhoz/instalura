81a36981f3dbb62b2b180ded7ab1510a08593a58
KaiqueMunhoz <munhoz13km@gmail.com>
2018-05-06 13:28:51 -0300

    Placed the components into this page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import Header from './../../components/Header';
+import Timeline from './../../components/Timeline';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-
+    <div id="root">
+      <div className="main">
+        <Header/>
+        <Timeline/>
       </div>
+    </div>
     );
   }
 }
